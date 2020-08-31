6df0bd787236db654228d4841cbedaf88a5f6719
igorizviekov <igorizviekov@gmail.com>
2020-08-31 14:03:47 +0300

    signature

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/index");
-router.get("/posts", controller.getSignature);
 module.exports = router;
 router.post("/signature", controller.getSignature);
